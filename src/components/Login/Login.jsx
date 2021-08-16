03ce1f87a38f99545917c5512d1ae44272d3721a
Ilya Borodulin <borodulinid@gmail.com>
2021-08-16 19:11:58 +0300

    feat: Login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,47 @@
+import './Login.scss';
+
+import block from 'bem-cn';
+
+import FormButton from '../UiKit/Buttons/FormButton/FormButton';
+import AuthInput from '../UiKit/Inputs/AuthInput/AuthInput';
+import DefaultAppLink from '../UiKit/Links/AppLink/DefaultAppLink/DefaultAppLink';
+import Logo from '../UiKit/Logo/Logo';
+
 const Login = () => {
+  const b = block('login');
+
   return (
-    <div className="login">
-      <h1>Страница "Логин"</h1>
-    </div>
+    <main className={b()}>
+      <section className={b('container')}>
+        <Logo className={b('logo')} />
+        <h1 className={b('title')}>Рады видеть!</h1>
+        <form className={b('form')} name="login" noValidate>
+          <fieldset className={b('inputs')}>
+            <AuthInput
+              mixClassName={b('input')}
+              type="email"
+              label="E-mail"
+              id={'email'}
+              required
+            />
+            <AuthInput
+              mixClassName={b('input')}
+              type="password"
+              label="Пароль"
+              id={'password'}
+              required
+            />
+          </fieldset>
+          <FormButton mixClassName={b('button')}>Войти</FormButton>
+          <div className={b('caption')}>
+            <p className={b('question')}>Ещё не зарегистрированы?</p>
+            <DefaultAppLink mixClassName={b('link')} to="/signup">
+              Регистрация
+            </DefaultAppLink>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 };
 
