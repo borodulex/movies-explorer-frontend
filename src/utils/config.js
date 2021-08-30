efd15bd8292e255f39499461afaed879f0589cdc
Ilya Borodulin <borodulinid@gmail.com>
2021-08-30 09:59:37 +0300

    feat: api address

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 export const MAIN_API_BASE_URL =
-  'http://localhost:3000/api';
+  'https://movies-explorer-borodulex.nomoredomains.club/api';
 
 export const MOVIES_API_BASE_URL = 'https://api.nomoreparties.co';
